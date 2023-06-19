48621ae7ecda56f81792094ea7f24120b8d648ab
Peter Gallwas <peter@husky.nz>
2023-06-20 10:32:08 +1200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function index({ user }) {
             <div className="flex flex-col items-center pb-10">
               <img
                 className="w-24 h-24 mb-3 rounded-full shadow-lg"
-                src="/blank-user-profile.jpg"
+                src="https://serv.filmclub.tech/wp-content/uploads/2023/06/19172257/logo128.png"
                 alt="Profile Image"
               />
               <h5 className="mb-1 text-xl font-medium text-gray-900 ">
